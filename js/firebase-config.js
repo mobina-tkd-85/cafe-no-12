ea8f2647943b8a65c6f383096f311b755e1112ae
Mobina_Hasanzadeh <mobina.tkd.85@gmail.com>
2026-08-26 12:36:00 +0330

    change the config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyB_RANVlz_RNRR54vovPHlCnsbflC8tEws",
   authDomain: "cafe-no-12.firebaseapp.com",
   projectId: "cafe-no-12",
   storageBucket: "cafe-no-12.firebasestorage.app",
   messagingSenderId: "644612037779",
-  appId: "1:644612037779:web:76f1c5e0b18e8611234166"
+  appId: "1:644612037779:web:76f1c5e0b18e8611234166",
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
